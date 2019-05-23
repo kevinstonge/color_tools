986377684271f35eeb5da676503f576bb381f021
Kevin St.Onge <kevinstonge@gmail.com>
2019-05-23 10:18:28 -0400

    build

---
+++ build/precache-manifest.eaaa6da5cf55c3e5cece1bb5d60fb709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bce224a9521e95dae778f52f70f9abf",
+    "url": "/color_tools/build/index.html"
+  },
+  {
+    "revision": "665c0ec1613f81aa573b",
+    "url": "/color_tools/build/static/css/main.8f264847.chunk.css"
+  },
+  {
+    "revision": "d27dcead68faee6d2b37",
+    "url": "/color_tools/build/static/js/2.44949046.chunk.js"
+  },
+  {
+    "revision": "665c0ec1613f81aa573b",
+    "url": "/color_tools/build/static/js/main.e8d34164.chunk.js"
+  },
+  {
+    "revision": "795a8a5c9ef1098b2b9f",
+    "url": "/color_tools/build/static/js/runtime~main.98bf1d36.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2c358c6643774cb423f90a2d0348932.js"
+  "/color_tools/build/precache-manifest.eaaa6da5cf55c3e5cece1bb5d60fb709.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color_tools/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
